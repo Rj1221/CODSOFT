409f2919451aceacd51687b3b0917bb73df18558
Raj <rj6207491172@gmail.com>
2023-08-03 06:32:18 +0530

    Little Change With Gui And Added KeyPress Events

--- Calculator/script.js
+++ Calculator/script.js
@@ -37,3 +37,18 @@ function deleteLastCharacter() {
     updateScreen();
 }
 updateScreen();
+function handleKeyPress(event) {
+    const key = event.key;
+
+    if (!isNaN(key) || key === "." || key === "+" || key === "-" || key === "*" || key === "/") {
+        appendToScreen(key);
+    } else if (key === "Enter") {
+        evaluateExpression();
+    } else if (key === "Delete" || key === "Backspace") {
+        deleteLastCharacter();
+    } else if (key === "Escape") {
+        clearScreen();
+    }
+}
+
+document.addEventListener("keydown", handleKeyPress);
